a6d1b7e8b4cb779fc89ce96032a9592b456d97f2
RuoJia0317 <cloudbay12@gmail.com>
2023-09-05 00:15:45 +0800

    update

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
